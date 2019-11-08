1a1010e6d7a2bc4b1847b51c61b27041f0e3957c
Ariana <=>
2019-11-08 17:16:00 -0200

    criando rotas e view

--- module/User/src/Controller/IndexController.php
+++ module/User/src/Controller/IndexController.php
@@ -9,16 +9,21 @@ class IndexController extends AbstractActionController
 {
     public function registerAction()
     {
+        $this->layout()->setTemplate('user/layout/layout');
         return new ViewModel();
     }
 
     public function recoveredPasswordAction()
     {
+        $this->layout()->setTemplate('user/layout/layout');
+
         return new ViewModel();
     }
 
     public function newPasswordAction()
     {
+        $this->layout()->setTemplate('user/layout/layout');
+
         return new ViewModel();
     }
 
