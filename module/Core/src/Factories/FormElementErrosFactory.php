3e5a91ec102b4c96b39d91fd7581aaa131042b42
Jesus Vieira <jesusvieiradelima@gmail.com>
2019-11-15 15:47:25 -0300

    ADD class FormElementErrors

--- module/Core/src/Factories/FormElementErrosFactory.php
+++ module/Core/src/Factories/FormElementErrosFactory.php
@@ -9,6 +9,7 @@ use Interop\Container\Exception\ContainerException;
 use Zend\ServiceManager\Exception\ServiceNotCreatedException;
 use Zend\ServiceManager\Exception\ServiceNotFoundException;
 
+use Zend\Form\View\Helper\FormElementErrors;
 
 class FormElementErrosFactory implements FactoryInterface
 {
@@ -20,18 +21,18 @@ class FormElementErrosFactory implements FactoryInterface
     {
         // TODO: Implement __invoke() method.
 
-        $helper = new FormElementErros();
+        $helper = new FormElementErrors();
 
         $config = $container->get('config');
-        if(isset($config['view_helper_config']['form_element_erross'])){
+        if (isset($config['view_helper_config']['form_element_erross'])) {
             $configHelper = $config['view_helper_config']['form_element_errors'];
-            if (isset($configHelper['message_open_format'])){
+            if (isset($configHelper['message_open_format'])) {
                 $helper->setMessageOpenFormat($configHelper['message_open_format']);
             }
-            if (isset($configHelper['message_separator_string'])){
+            if (isset($configHelper['message_separator_string'])) {
                 $helper->setMessageSeparatortring($configHelper['message_separator_string']);
             }
-            if (isset($configHelper['message_close_string'])){
+            if (isset($configHelper['message_close_string'])) {
                 $helper->setMessageCloseString($configHelper['message_close_string']);
             }
         }
